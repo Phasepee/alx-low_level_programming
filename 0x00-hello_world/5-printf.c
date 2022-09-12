337b4b2620ae87dd4549a2a6af62a018d74b5919
Phasepee <phasepee@gmail.com>
2022-09-12 05:38:34 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - entry point for the program.
+ * main - print the string in the function
  *
+ * Description: using the main function
  * Return:0 if no errors, return non zero if errors
- * /
+ */
 int main(void)
 {
-	    printf("with proper grammar, but the outcome is a piece of art,\n");
-	    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
