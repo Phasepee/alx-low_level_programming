64e1b0f5f7315b150380554ff9254a33813f0ed4
Phasepee <phasepee@gmail.com>
2022-09-12 06:25:02 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+
 /**
- *  *main -is the entry point for the program.
- *  *
- *  *Return:zero if no error, Non-zero value  if errors.
- *  */
+ * main - print the string in the main function
+ *
+ * Description: using the main function
+ * Return: 0 if no error, Non-zero value  if errors.
+ */
 int main(void)
 {
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %1d byte(s)\n", sizeof(c));
+	printf("Size of an int : %1d byte(s)\n", sizeof(i));
+	printf("Size of long int: %1d byte(s)\n", sizeof(li));
+	printf("size of long long int: %1d byte(s)\n", sizeof(lli));
+	printf("size of float: %1d byte(s)\n", sizeof(f));
+	return (0);
+}
