396836c4bec2488cd20a400ff096466f707c2584
Phasepee <phasepee@gmail.com>
2022-09-12 06:51:42 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,8 +16,8 @@ int main(void)
 
 	printf("Size of a char: %d byte(s)\n", sizeof(c));
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
-	printf("Size of long int: %d byte(s)\n", sizeof(li));
-	printf("size of long long int: %d byte(s)\n", sizeof(lli));
-	printf("size of float: %d byte(s)\n", sizeof(f));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("size of a float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
