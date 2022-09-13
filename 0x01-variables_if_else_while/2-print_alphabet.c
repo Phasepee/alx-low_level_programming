515a2a35c214acb54f597424d0e1bb650c07d9e2
Phasepee <phasepee@gmail.com>
2022-09-12 21:19:34 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+@@ -1,21 +1,22 @@
+/*
+ *  * File: 2-print_alphabet.c
+ *   * Auth: Brennan D Baraban
+ *    */
+#include <stdio.h>
+/**
+ *  * main - Prints the alphabet in lowercase.
+ *   *
+ *    * Return: 0 if successful, non-zero otherwise.
+ *     * Return: Always 0.
+ *      */
+int main(void)
+{
+		char letter;
+
+			for (letter = 'a'; letter <= 'z'; letter++)
+						putchar(letter);
+
+				putchar('\n');
+
+					return (0);
+}
