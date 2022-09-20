142cb17bd3535489b47b174ec712febf3e707f61
Phasepee <phasepee@gmail.com>
2022-09-20 10:37:53 -0700

    T 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,5 @@
-/*
- * File: 2-print_alphabet.c 
- * Auth: Brennan D Baraban
- */
 #include <stdio.h>
+
 /**
  * main - Prints the alphabet in lowercase.
  *
@@ -10,12 +7,12 @@
  */
 int main(void)
 {
-		char letter;
+	char letter;
 
-		for (letter = 'a'; letter <= 'z'; letter++)
-						putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+					putchar(letter);
 
-		putchar('\n');
+	putchar('\n');
 
-		return (0);
+	return (0);
 }
