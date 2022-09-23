cccdbad95fc348adf343ef54e2069d28b545bd47
Phasepee <phasepee@gmail.com>
2022-09-23 02:35:40 -0700

    T 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,40 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/**
+ *
+ * File: main.h
+ * Auth: sam tech(Samuel birhanu)
+ * Desc: Header file containing prototype for all functions used in
+ *	the 0x06-pointers_arrays_strings directory.
+ */
+
+int _putchar(char c);
+
+char *strcat(char *dest, *src);
+
+char *strncat(char *dest, *src, int n);
+
+char *strncpy(char *dest, *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
+
+char *leet(char *);
+
+char *rot13(char *);
+
+void print_number(int n);
+
+char *infinite_add(char *n1, char *2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
+
+
+#endif
