fa64900ff33254c7fbfd7206d2df2a8270b8665d
Phasepee <phasepee@gmail.com>
2022-09-26 12:06:53 -0700

    T 0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+/**
+ * File: main.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
