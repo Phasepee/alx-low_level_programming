4c494b6121fc73959bf05b8f55987db0888acba7
Phasepee <phasepee@gmail.com>
2022-09-26 21:17:42 -0700

    T 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
  * Auth: Brennan D Baraban
  */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _memset - Fills the first n bytes of the memory area
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 /**
  * File: main.h
  * Auth: Brennan D Baraban
@@ -7,4 +10,12 @@
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcp(char *dest, char *scr, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8];
+void print_diagsums(int *a, int size);
 
+#endif
