843cf443637e0387a91a83d09bd6fbfcce30b43c
Phasepee <phasepee@gmail.com>
2022-09-28 09:32:00 -0700

    T 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+#include <stdlib.h>
+
 int _putchar(char c);
-void reset_to_98(int *n)
-void swap_int(char *a, int *b);
-int _strlen(char *str);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -10,3 +16,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+#endif
