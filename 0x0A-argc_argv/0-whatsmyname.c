2ca4730eee2dcc6b713ed1badfec3f3e546b38dd
Phasepee <phasepee@gmail.com>
2022-09-30 05:03:24 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print a programe name, followed by anew line.
+ * @argc: The number of arguments supplied to the programe.
+ * @argv: An array of pointers to arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) arc, char *argv[])
+{
+	printf("%s\n", argv[0];
+
+	return (0);
+}
